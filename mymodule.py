b89df8179bf2a122a706f5fdaf9438f4ecf0c692
Sameera <sshaik80676@gmail.com>
2026-08-05 23:00:05 +0530

    modules

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    c=a+b
+    return c
+def sub(c,d):
+    s=c-d
+    return s
+def mul(e,f):
+    l=e*f
+    return l
+def div(g,h):
+    p=g/h
+    return p 
+
+print(add(10,20))
+print(sub(30,10))
+print(mul(5,6))
+print(div(100,10))
---
+++ test.py
@@ -0,0 +1,7 @@
+#import mymodule
+#import mymodule as mm
+#mm.add(10,20)
+#mm.sub(30,10)
+#mm.mul(5,6)
+#mm.div(100,10)
+from mymodule import add,sub,mul,div
